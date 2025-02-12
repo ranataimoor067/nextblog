e7e48ccbe6a4dccbcb8411a961f9d5318b485439
Taimoor <ranatamoor067@gmail.com>
2025-02-12 21:59:32 +0500

    Your commit message

--- src/app/post/[slug]/page.jsx
+++ src/app/post/[slug]/page.jsx
@@ -1,146 +1,61 @@
-"use client"; // Add this line
-import { Alert, Button, FileInput, Select, TextInput } from 'flowbite-react';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
-import { useState } from 'react';
-import { CircularProgressbar } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-
-export default function CreatePost() {
-  const [file, setFile] = useState(null);
-  const [imageUploadProgress, setImageUploadProgress] = useState(null);
-  const [imageUploadError, setImageUploadError] = useState(null);
-  const [formData, setFormData] = useState({});
-  const [publishError, setPublishError] = useState(null);
-  const navigate = useNavigate();
-
-  const handleUploadImage = async () => {
-    try {
-      if (!file) {
-        setImageUploadError('Please select an image');
-        return;
-      }
-      setImageUploadError(null);
-      const formData = new FormData();
-      formData.append('file', file);
-
-      // Hardcoded Cloudinary credentials
-      const cloudName = 'dpwo3i4ea'; // Replace with your Cloudinary cloud name
-      const uploadPreset = 'taimoor2'; // Replace with your Cloudinary upload preset
-
-      const response = await axios.post(
-        `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
-        formData,
-        {
-          params: { upload_preset: uploadPreset },
-          onUploadProgress: (progressEvent) => {
-            const progress = Math.round(
-              (progressEvent.loaded * 100) / progressEvent.total
-            );
-            setImageUploadProgress(progress);
-          },
-        }
-      );
-
-      const downloadURL = response.data.secure_url;
-      setImageUploadProgress(null);
-      setFormData({ ...formData, image: downloadURL });
-    } catch (error) {
-      console.error(error);
-      setImageUploadError('Image upload failed');
-      setImageUploadProgress(null);
-    }
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const res = await fetch('/api/post/create', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(formData),
-      });
-      const data = await res.json();
-      if (!res.ok) {
-        setPublishError(data.message);
-        return;
-      }
-      navigate(`/post/${data.slug}`);
-    } catch (error) {
-      setPublishError('Something went wrong');
-    }
-  };
-
+import CallToAction from '@/app/components/CallToAction';
+import RecentPosts from '@/app/components/RecentPosts';
+import { Button } from 'flowbite-react';
+import Link from 'next/link';
+export default async function PostPage({ params }) {
+  let post = null;
+  try {
+    const result = await fetch(process.env.URL + '/api/post/get', {
+      method: 'POST',
+      body: JSON.stringify({ slug: params.slug }),
+      cache: 'no-store',
+    });
+    const data = await result.json();
+    post = data.posts[0];
+  } catch (error) {
+    post = { title: 'Failed to load post' };
+  }
+  if (!post || !post.title === 'Failed to load post') {
+    return (
+      <main className='p-3 flex flex-col max-w-6xl mx-auto min-h-screen'>
+        <h2 className='text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl'>
+          Post not found
+        </h2>
+      </main>
+    );
+  }
   return (
-    <div className='p-3 max-w-3xl mx-auto min-h-screen'>
-      <h1 className='text-center text-3xl my-7 font-semibold'>Create a post</h1>
-      <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
-        <div className='flex flex-col gap-4 sm:flex-row justify-between'>
-          <TextInput
-            type='text'
-            placeholder='Title'
-            required
-            id='title'
-            className='flex-1'
-            onChange={(e) => setFormData({ ...formData, title: e.target.value })}
-          />
-          <Select
-            onChange={(e) => setFormData({ ...formData, category: e.target.value })}
-          >
-            <option value='uncategorized'>Select a category</option>
-            <option value='javascript'>JavaScript</option>
-            <option value='reactjs'>React.js</option>
-            <option value='nextjs'>Next.js</option>
-          </Select>
-        </div>
-        <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
-          <FileInput
-            type='file'
-            accept='image/*'
-            onChange={(e) => setFile(e.target.files[0])}
-          />
-          <Button
-            type='button'
-            gradientDuoTone='purpleToBlue'
-            size='sm'
-            outline
-            onClick={handleUploadImage}
-            disabled={imageUploadProgress}
-          >
-            {imageUploadProgress ? (
-              <div className='w-16 h-16'>
-                <CircularProgressbar
-                  value={imageUploadProgress}
-                  text={`${imageUploadProgress}%`}
-                />
-              </div>
-            ) : (
-              'Upload Image'
-            )}
-          </Button>
-        </div>
-        {imageUploadError && <Alert color='failure'>{imageUploadError}</Alert>}
-        {formData.image && (
-          <img
-            src={formData.image}
-            alt='upload'
-            className='w-full h-72 object-cover'
-          />
-        )}
-        <ReactQuill
-          theme='snow'
-          placeholder='Write something...'
-          className='h-72 mb-12'
-          required
-          onChange={(value) => setFormData({ ...formData, content: value })}
-        />
-        <Button type='submit' gradientDuoTone='purpleToPink'>
-          Publish
+    <main className='p-3 flex flex-col max-w-6xl mx-auto min-h-screen'>
+      <h1 className='text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl'>
+        {post && post.title}
+      </h1>
+      <Link
+        href={`/search?category=${post && post.category}`}
+        className='self-center mt-5'
+      >
+        <Button color='gray' pill size='xs'>
+          {post && post.category}
         </Button>
-        {publishError && <Alert color='failure'>{publishError}</Alert>}
-      </form>
-    </div>
+      </Link>
+      <img
+        src={post && post.image}
+        alt={post && post.title}
+        className='mt-10 p-3 max-h-[600px] w-full object-cover'
+      />
+      <div className='flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs'>
+        <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
+        <span className='italic'>
+          {post && (post?.content?.length / 1000).toFixed(0)} mins read
+        </span>
+      </div>
+      <div
+        className='p-3 max-w-2xl mx-auto w-full post-content'
+        dangerouslySetInnerHTML={{ __html: post?.content }}
+      ></div>
+      <div className='max-w-4xl mx-auto w-full'>
+        <CallToAction />
+      </div>
+      <RecentPosts limit={3} />
+    </main>
   );
 }
